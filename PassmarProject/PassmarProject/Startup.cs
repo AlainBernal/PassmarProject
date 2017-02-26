1052d1a7f1066a38b9b0b04f1ac3fd39cde6e485
AlainBernal <jahirbgonzalez@gmail.com>
2017-02-26 17:12:21 -0600

    Inicializacion de Proyecto
    Archibos base

---
+++ PassmarProject/PassmarProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PassmarProject.Startup))]
+namespace PassmarProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
